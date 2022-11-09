db5ebef1c743c127b83b668d5c6a274084029457
Diego Pereira Grassato <diego.grassato@bradesco.com.br>
2022-11-09 11:45:49 -0300

    Adiciona versao 1.0.0

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+
+import WebViewExample from './web_view_example.js';
+
+const App = () => {
+
+   return (
+      <WebViewExample/>
+   )
+}
+
+export default App;
+
---
+++ README.md
@@ -0,0 +1,2 @@
+npx expo install expo-loca'tion
+npx expo install expo-background-fetch
